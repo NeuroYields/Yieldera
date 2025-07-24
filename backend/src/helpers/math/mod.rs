013f830ea5d353cd4e1e961599034d973fbd2782
Ayoub Amer <ayouub.ameur@gmail.com>
2025-07-24 13:04:25 +0100

    feat(backend): add  math fn to convert a price to its tick

--- backend/src/helpers/math/mod.rs
+++ backend/src/helpers/math/mod.rs
@@ -17,3 +17,25 @@ pub fn tick_to_price(tick: i32, token0_decimals: u8, token1_decimals: u8) -> Res
 pub fn align_to_pool_tick_spacing(tick: i32, spacing: i32) -> i32 {
     tick - (tick % spacing)
 }
+
+pub fn convert_price_to_tick(
+    price: f64,
+    token0_decimals: u8,
+    token1_decimals: u8,
+    tick_spacing: i32,
+) -> Result<i32> {
+    let tick = price1_to_tick(price, token0_decimals, token1_decimals);
+
+    // Align the tick to the pool tick spacing
+    let aligned_tick = align_to_pool_tick_spacing(tick, tick_spacing);
+
+    Ok(aligned_tick)
+}
+
+pub fn price1_to_tick(price: f64, token0_decimals: u8, token1_decimals: u8) -> i32 {
+    let diff_decimals = token1_decimals as i8 - token0_decimals as i8;
+    let adjusted_price = price * 10_f64.powi(diff_decimals as i32);
+
+    let log_base = 1.0001_f64.ln(); // natural log of 1.0001
+    (adjusted_price.ln() / log_base).floor() as i32
+}
