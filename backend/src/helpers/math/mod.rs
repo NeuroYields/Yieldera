9c255d63d33e294dafdee5d01ecbdcb4df42942b
Ayoub Amer <ayouub.ameur@gmail.com>
2025-07-24 14:02:44 +0100

    feat(backend): add estimate amount1 from amount0 on adding liqudity

--- backend/src/helpers/math/mod.rs
+++ backend/src/helpers/math/mod.rs
@@ -1,4 +1,12 @@
+pub mod uniswap_v3;
+
+use alloy::primitives::U256;
 use color_eyre::eyre::Result;
+
+use crate::helpers::math::uniswap_v3::{
+    liquidity_math::{get_amount1_for_liquidity, get_liquidity_for_amount0},
+    tick_math::get_sqrt_ratio_at_tick,
+};
 
 /// Convert a tick to a price.
 /// It caclulate the price1 which the price of token1 per token0
@@ -38,4 +46,22 @@ pub fn price1_to_tick(price: f64, token0_decimals: u8, token1_decimals: u8) -> i
 
     let log_base = 1.0001_f64.ln(); // natural log of 1.0001
     (adjusted_price.ln() / log_base).floor() as i32
+}
+
+pub fn estimate_amount1_given_amount0(
+    sqrt_price_x96: U256,
+    tick_lower: i32,
+    tick_upper: i32,
+    amount0: U256,
+) -> Result<U256> {
+    let sqrt_lower = get_sqrt_ratio_at_tick(tick_lower).unwrap();
+    let sqrt_upper = get_sqrt_ratio_at_tick(tick_upper).unwrap();
+
+    // Estimate liquidity using amount0
+    let liquidity = get_liquidity_for_amount0(sqrt_price_x96, sqrt_upper, amount0)?;
+
+    // Estimate how much token1 is needed
+    let amount1 = get_amount1_for_liquidity(sqrt_price_x96, sqrt_lower, liquidity)?;
+
+    Ok(amount1)
 }
