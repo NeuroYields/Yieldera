de4e57577d33c40a1477f2b516d070b29825b10f
Ayoub Amer <ayouub.ameur@gmail.com>
2025-07-29 18:27:22 +0100

    feat(contract): deploy multiple vauls with same script

--- contracts/script/DeployYielderaVault.s.sol
+++ contracts/script/DeployYielderaVault.s.sol
@@ -5,17 +5,24 @@ import "forge-std/Script.sol";
 import "../src/YielderaVault.sol"; // adjust path if needed
 
 contract DeployYieldraVault is Script {
-    function run() external returns (address) {
+    function run() external {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(pk);
 
         // SAUCE-HBAR pool on tetsnet saucerswap
-        address pool = 0x37814eDc1ae88cf27c0C346648721FB04e7E0AE7;
+        address pool1 = 0x37814eDc1ae88cf27c0C346648721FB04e7E0AE7;
 
-        YielderaVault helper = new YielderaVault(pool);
+        YielderaVault vault1 = new YielderaVault(pool1);
+
+        console.log("YielderaVault1 deployed to:", address(vault1));
+
+        // USDC-DAI pool on testnet saucerswap
+        address pool2 = 0xb431866114B634F611774ec0d094BF11cb91c7E4;
+
+        YielderaVault vault2 = new YielderaVault(pool2);
+
+        console.log("YielderaVault2 deployed to:", address(vault2));
 
         vm.stopBroadcast();
-        console.log("YielderaVault deployed to:", address(helper));
-        return address(helper);
     }
 }
