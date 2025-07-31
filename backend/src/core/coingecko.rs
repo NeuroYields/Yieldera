a138f85064a3e13adcaba5df512e94ce160d8e2e
Ayoub Amer <ayouub.ameur@gmail.com>
2025-07-31 13:59:06 +0100

    fix(backend): fetch coingecko by base token0 instead of 1

--- backend/src/core/coingecko.rs
+++ backend/src/core/coingecko.rs
@@ -10,11 +10,11 @@ pub async fn get_pool_ohlcv_data(
     let network_id = "hedera-hashgraph";
     // let network_id = "sei-evm";
     let pool_address = pool_address.to_lowercase();
-    let token_1_address = vault_details.pool.token1.address.as_str();
+    let token_0_address = vault_details.pool.token0.address.as_str();
 
     let url = format!(
         "https://api.coingecko.com/api/v3/onchain/networks/{}/pools/{}/ohlcv/day?limit=1000&currency=token&token={}&include_empty_intervals=false",
-        network_id, pool_address, token_1_address
+        network_id, pool_address, token_0_address
     );
 
     let coingecko_api_key = &CONFIG.coingecko_api_key;
